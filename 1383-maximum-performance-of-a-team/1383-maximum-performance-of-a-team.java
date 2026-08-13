a369d842da63383e0c714e834cf9dcac2bcd8988
ANISH SAMANT <anish.samant97@gmail.com>
2026-08-13 04:52:08 -0700

    Time: 42 ms (33.84%), Space: 65.9 MB (96.35%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -1,23 +1,24 @@
 class Solution {
     public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
         List<int[]> myList = new ArrayList<>();
+
         for (int i = 0; i < speed.length; i++) {
             myList.add(new int[]{speed[i], efficiency[i]});
         }
 
         myList.sort((a,b) -> b[1] - a[1]);
 
+        PriorityQueue<Integer> myQueue = new PriorityQueue<>();
         long speedSum = 0;
         long maxPerf = 0;
-        PriorityQueue<Integer> speeds = new PriorityQueue<>();
-        for (int i = 0; i < myList.size(); i++) {
-            int spd = myList.get(i)[0];
-            int eff = myList.get(i)[1];
-            speedSum += spd;
-            speeds.offer(spd);
 
-            if (speeds.size() > k) {
-                speedSum -= speeds.poll();
+        for (int[] eng : myList) {
+            int spd = eng[0], eff = eng[1];
+            speedSum += spd;
+
+            myQueue.offer(spd);
+            if (myQueue.size() > k) {
+                speedSum -= myQueue.poll();
             }
 
             maxPerf = Math.max(maxPerf, speedSum * eff);

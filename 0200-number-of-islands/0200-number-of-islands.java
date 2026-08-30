4a725be3576dfa53ae37813eb3a7c324c2ce4387
ANISH SAMANT <anish.samant97@gmail.com>
2026-08-30 12:28:52 -0700

    Time: 5 ms (29.08%), Space: 51.1 MB (95.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -13,6 +13,22 @@ class Solution {
         }
     }
 
+    public void bfs(char[][] grid, int i, int j) {
+        Queue<int[]> myQueue = new LinkedList<>();
+        myQueue.offer(new int[]{i, j});
+        while (!myQueue.isEmpty()) {
+            int[] curr = myQueue.poll();
+            for (int[] dir : dirs) {
+                int newi = curr[0] + dir[0];
+                int newj = curr[1] + dir[1];
+                if (newi >= 0 && newi < m && newj >= 0 && newj < n && grid[newi][newj] == '1') {
+                    grid[newi][newj] = '0';
+                    myQueue.offer(new int[]{newi, newj});
+                }
+            }
+        }
+    }
+
     public int numIslands(char[][] grid) {
         m = grid.length;
         n = grid[0].length;
@@ -21,7 +37,8 @@ class Solution {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
                     count++;
-                    dfs(grid, i, j);
+                    // dfs(grid, i, j);
+                    bfs(grid, i, j);
                 }
             }
         }

1ea4b1c42df0f78984bd131a4a25b808c986cf01
ANISH SAMANT <anish.samant97@gmail.com>
2026-08-30 10:29:41 -0700

    Time: 26 ms (74.07%), Space: 47.4 MB (96.3%) - LeetHub

---
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long numberOfWays(String s) {
+        // 101
+        // 010
+        long res = 0;
+        long[] cnt = new long[2];
+        for (int i = 0; i < s.length(); i++) {
+            cnt[s.charAt(i) - '0']++;
+        }
+
+        long currZeros = 0, currOnes = 0;
+        if (s.charAt(0) == '0') currZeros++;
+        else currOnes++;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                res += (currOnes * (cnt[1] - currOnes));
+                currZeros++;
+            } else {
+                res += (currZeros * (cnt[0] - currZeros));
+                currOnes++;
+            }
+        }
+
+        return res;
+    }
+}

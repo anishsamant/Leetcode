3661377c00fe931fe4727d8dee8d8de24190babf
ANISH SAMANT <anish.samant97@gmail.com>
2026-08-30 12:29:17 -0700

    Time: 3 ms (88.01%), Space: 52.3 MB (45.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -37,8 +37,8 @@ class Solution {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
                     count++;
-                    // dfs(grid, i, j);
-                    bfs(grid, i, j);
+                    dfs(grid, i, j);
+                    // bfs(grid, i, j);
                 }
             }
         }

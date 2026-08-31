4e56592883c0125c351e82f2bd69bdfb3c344edd
ANISH SAMANT <anish.samant97@gmail.com>
2026-08-30 20:10:40 -0700

    Time: 26 ms (97.2%), Space: 143.5 MB (75.62%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,24 +1,30 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        // index, value
-        PriorityQueue<int[]> myQueue = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-
-        for (int i = 0; i < k; i++) {
-            myQueue.offer(new int[]{i, nums[i]});
-        }
-
+        Deque<Integer> dq = new ArrayDeque<>();
         int len = nums.length - k + 1;
         int[] ans = new int[len];
-        ans[0] = myQueue.peek()[1];
+
+        for (int i = 0; i < k; i++) {
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+        }        
+
+        ans[0] = nums[dq.peekFirst()];
 
         int l = 1;
-        for (int i = k; i < nums.length; i++) {
-            while (!myQueue.isEmpty() && myQueue.peek()[0] <= i - k) {
-                myQueue.poll();
+        for (int r = k; r < nums.length; r++) {
+            if (dq.peekFirst() <= r - k) {
+                dq.pollFirst();
             }
 
-            myQueue.offer(new int[]{i, nums[i]});
-            ans[l++] = myQueue.peek()[1];
+            while (!dq.isEmpty() && nums[r] >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+
+            dq.offerLast(r);
+            ans[l++] = nums[dq.peekFirst()];
         }
 
         return ans;

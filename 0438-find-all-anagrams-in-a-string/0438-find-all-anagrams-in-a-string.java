8f1fce0d45996289cd0d3e2d470240038a0adf44
ANISH SAMANT <anish.samant97@gmail.com>
2026-08-31 11:53:30 -0700

    Time: 8 ms (95.55%), Space: 46.8 MB (58.17%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -2,23 +2,34 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         if (s.length() < p.length()) return new ArrayList<>();
 
-        Map<Character, Integer> countP = new HashMap<>();
-        Map<Character, Integer> window = new HashMap<>();
+        // Map<Character, Integer> countP = new HashMap<>();
+        // Map<Character, Integer> window = new HashMap<>();
+        int[] window = new int[26];
+        int[] countP = new int[26];
 
         for (int i = 0; i < p.length(); i++) {
-            countP.put(p.charAt(i), countP.getOrDefault(p.charAt(i), 0) + 1);
+            // countP.put(p.charAt(i), countP.getOrDefault(p.charAt(i), 0) + 1);
+            countP[p.charAt(i) - 'a']++;
+        }
+
+        int need = 0;
+        for (int i = 0; i < 26; i++) {
+            if (countP[i] > 0) need++;
         }
 
         int l = 0;
-        int have = 0, need = countP.size();
+        int have = 0;
+        // int need = countP.size();
         List<Integer> ans = new ArrayList<>();
         for (int r = 0; r < s.length(); r++) {
             char c = s.charAt(r);
-            window.put(c, window.getOrDefault(c, 0) + 1);
+            // window.put(c, window.getOrDefault(c, 0) + 1);
+            window[c - 'a']++;
 
-            if (countP.containsKey(c) && countP.get(c).equals(window.get(c))) {
-                have++;
-            }
+            // if (countP.containsKey(c) && countP.get(c).equals(window.get(c))) {
+            //     have++;
+            // }
+            if (countP[c - 'a'] == window[c - 'a']) { have++; }
 
             while (have == need) {
                 if (r - l + 1 == p.length()) {
@@ -26,11 +37,13 @@ class Solution {
                 }
 
                 char lc = s.charAt(l);
-                window.put(lc, window.get(lc) - 1);
+                //window.put(lc, window.get(lc) - 1);
+                window[lc - 'a']--;
                 l++;
-                if (countP.containsKey(lc) && window.get(lc) < countP.get(lc)) {
-                    have--;
-                }
+                // if (countP.containsKey(lc) && window.get(lc) < countP.get(lc)) {
+                //     have--;
+                // }
+                if (window[lc - 'a'] < countP[lc - 'a']) { have--; }
             }
         }
 

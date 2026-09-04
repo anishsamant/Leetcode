9b2b25eb7b3d1a6aedc825a9175d6ba81ea0fc1d
ANISH SAMANT <anish.samant97@gmail.com>
2026-09-04 11:08:28 -0700

    Time: 27 ms (69.73%), Space: 47 MB (79.43%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        // deeedbbcccbdaa
+        Stack<Integer> counts = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int sbLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (sbLen == 0 || s.charAt(i) != sb.charAt(sbLen - 1)) {
+                counts.push(1);
+                sb.append(s.charAt(i));
+                sbLen++;
+            } else {
+                int count = counts.pop() + 1;
+                if (count == k) {
+                    sb.delete(sbLen - (k - 1), sbLen);
+                    sbLen -= (k - 1);
+                } else {
+                    counts.push(count);
+                    sb.append(s.charAt(i));
+                    sbLen++;
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}

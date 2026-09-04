a23b5992ec12c15230e8094a449f7cd78a96bdf3
ANISH SAMANT <anish.samant97@gmail.com>
2026-09-04 02:58:01 -0700

    Time: 2 ms (81.42%), Space: 43.9 MB (42.52%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -8,22 +8,25 @@ class Solution {
             lastOccurence.put(s.charAt(i), i);
         }
 
+        StringBuilder sb = new StringBuilder();
+        int sbLen = 0;
         for (int i = 0; i < s.length(); i++) {
             char curr = s.charAt(i);
             
             if (seen[curr - 'a'] == 0) {
-                while (!myQueue.isEmpty() && curr < myQueue.peekLast() && lastOccurence.get(myQueue.peekLast()) > i) {
-                    char top = myQueue.pollLast();
-                    seen[top - 'a'] = 0;
+                while (sbLen > 0 && curr < sb.charAt(sbLen - 1) && lastOccurence.get(sb.charAt(sbLen - 1)) > i) {
+                    char prev = sb.charAt(sbLen - 1);
+                    sb.deleteCharAt(sbLen - 1);
+                    sbLen--;
+                    seen[prev - 'a'] = 0;
                 }
-                myQueue.offerLast(curr);
+                sb.append(curr);
                 seen[curr - 'a'] = 1;
+                sbLen++;
             }
 
         }
 
-        StringBuilder sb = new StringBuilder(myQueue.size());
-        while (!myQueue.isEmpty()) { sb.append(myQueue.pollFirst()); }
         return sb.toString();
     }
 }

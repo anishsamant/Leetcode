eaf81ffff152f859024c56bcb207a89350b8bc5d
ANISH SAMANT <anish.samant97@gmail.com>
2026-09-04 11:12:39 -0700

    Time: 25 ms (74.02%), Space: 46.7 MB (93.15%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -2,23 +2,22 @@ class Solution {
     public String removeDuplicates(String s, int k) {
         // deeedbbcccbdaa
         Stack<Integer> counts = new Stack<>();
-        StringBuilder sb = new StringBuilder(s);
-        // int sbLen = 0;
-        for (int i = 0; i < sb.length(); i++) {
-            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+        StringBuilder sb = new StringBuilder();
+        int sbLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (sbLen == 0 || s.charAt(i) != sb.charAt(sbLen - 1)) {
                 counts.push(1);
-                // sb.append(s.charAt(i));
-                // sbLen++;
+                sb.append(s.charAt(i));
+                sbLen++;
             } else {
                 int count = counts.pop() + 1;
                 if (count == k) {
-                    sb.delete(i - k + 1, i + 1);
-                    i = i - k;
-                    //sbLen -= (k - 1);
+                    sb.delete(sbLen - (k - 1), i + 1);
+                    sbLen -= (k - 1);
                 } else {
                     counts.push(count);
-                    // sb.append(s.charAt(i));
-                    // sbLen++;
+                    sb.append(s.charAt(i));
+                    sbLen++;
                 }
             }
         }
